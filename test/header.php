bb51f5cd681af4c8559a978ab0ad58f7775eb211
ponpon <ponponumi@gmail.com>
2024-04-04 00:53:30 +0900

    コンストラクタのテストを追加

--- test/header.php
+++ test/header.php
@@ -26,8 +26,19 @@ $options = [
   ],
 ];
 
+$option = [
+  "selector" => ".box",
+  "property" => [
+    "background-color" => "#e321d2",
+    "width" => "100%",
+    "height" => "30px",
+    "border-radius" => "5px",
+  ],
+];
+
 // コンストラクタのテストを実行
 $styleClass = new StyleCreate($options,true);
+$styleClass2 = new StyleCreate($option);
 
 ?>
 <!DOCTYPE html>
@@ -37,5 +48,6 @@ $styleClass = new StyleCreate($options,true);
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <?php $styleClass->getStyle(true) ?>
+  <?php $styleClass2->getStyle(true) ?>
 </head>
 <body>
